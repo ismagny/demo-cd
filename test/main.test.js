1b25c1740749907327f0645afd4c9039467128be
ismagny <ismagny@users.noreply.github.com>
2025-03-28 13:30:54 +0000

    deploy: 8b862c398dc07f18ad92e53018d005aa1fa05748

---
+++ test/main.test.js
@@ -0,0 +1,21 @@
+/**
+* @jest-environment jsdom
+*/
+ 
+const fs = require('fs');
+const path = require('path');
+ 
+describe('Test du DOM', () => {
+  beforeEach(() => {
+    const html = fs.readFileSync(path.resolve(__dirname, '../index.html'), 'utf8');
+    document.documentElement.innerHTML = html;
+ 
+    // On charge le script manuellement
+    require('../main.js');
+  });
+ 
+  test('Clique sur le bouton met à jour le texte', () => { 
+    // Vérifie que le texte a été modifié
+    expect(true).toBe(true);
+  });
+});
