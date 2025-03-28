8864f3d9d0de04a2bdee832410cc3da29b9b9836
Ismail GUNEY <ismail.guney2023@campus-eni.fr>
2025-03-28 14:36:02 +0100

    pokemon api

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 console.log("Ouiiiiiiii ça marche 😊");
+
+const url = "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=40"
+
+fetch(url)
+.then(response => response.json())
+.then(pokemons => {
+    console.log(pokemons);
+})
